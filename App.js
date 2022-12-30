eaaf72a68ae67a956988340b643b0a153d4fb6c8
Nguyen Tuan <tuanbeo203@gmail.com>
2022-12-30 19:49:17 +0700

    new changes

--- App.js
+++ App.js
@@ -5,23 +5,22 @@ import {NavigationContainer} from '@react-navigation/native';
 import RootMain from './src/root/rootMain';
 import LoginScreens from './src/components/Screens/login/loginScreens';
 import ShowProductScreen from './src/components/Screens/home/showProduct/showProduct';
-
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Login"
-          screenOptions={{
-            headerShown: false,
-          }}>
-          <Stack.Screen name="Login" component={LoginScreens} />
-          <Stack.Screen name="RootMain" component={RootMain} />
-          <Stack.Screen name="showProduct" component={ShowProductScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Login"
+            screenOptions={{
+              headerShown: false,
+            }}>
+            <Stack.Screen name="Login" component={LoginScreens} />
+            <Stack.Screen name="RootMain" component={RootMain} />
+            <Stack.Screen name="showProduct" component={ShowProductScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
     </View>
   );
 };
--- src/root/rootMain.js
+++ src/root/rootMain.js
@@ -5,68 +5,76 @@ import Home from '../components/Screens/home/home';
 import CartScreen from '../components/Screens/cart/cart';
 import FavoriteScreen from '../components/Screens/favorite/favorite';
 import UserScreen from '../components/Screens/user/user';
-
 const Tab = createBottomTabNavigator();
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import HomeIcon from 'react-native-vector-icons/SimpleLineIcons';
+import FavoriteIcon from 'react-native-vector-icons/MaterialIcons';
+import CartIcon from 'react-native-vector-icons/Ionicons';
+import UserIcon from 'react-native-vector-icons/FontAwesome';
+
 const RootMain = () => {
   return (
     <View style={styles.container}>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerShown: false,
-          tabBarStyle: {
-            backgroundColor: '#9775FA',
-            borderRadius: 20,
-            marginBottom: 7,
-            marginHorizontal: 10,
-          },
-
-          tabBarShowLabel: false,
-          tabBarHideOnKeyboard: true,
-          tabBarActiveTintColor: '#00FF47',
-          tabBarInactiveTintColor: 'white',
-        }}>
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{
-            // tabBarIcon: ({color}) => {
-            //   return <MaterialIcons name="home" size={30} color={color} />;
-            // },
-          }}
-        />
-        <Tab.Screen
-          name="Favorite"
-          component={FavoriteScreen}
-          options={{
-            // tabBarIcon: ({color}) => {
-            //   return <MaterialIcons name="favorite" size={30} color={color} />;
-            // },
-          }}
-        />
-        <Tab.Screen
-          name="CartScreen"
-          component={CartScreen}
-          options={{
-            // tabBarIcon: ({color}) => {
-            //   return (
-            //     <MaterialIcons name="shopping-cart" size={30} color={color} />
-            //   );
-            // },
-          }}
-        />
-        <Tab.Screen
-          name="User"
-          component={UserScreen}
-          options={{
-            // tabBarIcon: ({color}) => {
-            //   return (
-            //     <MaterialIcons name="account-circle" size={30} color={color} />
-            //   );
-            // },
-          }}
-        />
-      </Tab.Navigator>
+      <SafeAreaProvider>
+        <Tab.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerShown: false,
+            tabBarStyle: {
+              backgroundColor: '#9775FA',
+              borderRadius: 20,
+              marginBottom: 7,
+              marginHorizontal: 10,
+            },
+            tabBarShowLabel:false,
+            tabBarHideOnKeyboard: true,
+            tabBarActiveTintColor: '#00FF47',
+            tabBarInactiveTintColor: 'white',
+          }}>
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              tabBarIcon: ({color}) => {
+                return <HomeIcon name="home" size={25} color={color} />;
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Favorite"
+            component={FavoriteScreen}
+            options={
+              {
+                tabBarIcon: ({color}) => {
+                  return <FavoriteIcon name="favorite-outline" size={28} color={color} />;
+                },
+              }
+            }
+          />
+          <Tab.Screen
+            name="CartScreen"
+            component={CartScreen}
+            options={
+              {
+                tabBarIcon: ({color}) => {
+                  return <CartIcon name="cart-outline" size={30} color={color} />;
+                },
+              }
+            }
+          />
+          <Tab.Screen
+            name="User"
+            component={UserScreen}
+            options={
+              {
+                tabBarIcon: ({color}) => {
+                  return <UserIcon name="user-o" size={25} color={color} />;
+                },
+              }
+            }
+          />
+        </Tab.Navigator>
+      </SafeAreaProvider>
     </View>
   );
 };
