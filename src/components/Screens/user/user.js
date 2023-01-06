d3a68859950ecf847d6b8367e43c91abec3dd1a8
Nguyen Tuan <tuanbeo203@gmail.com>
2023-01-06 12:12:04 +0700

    new changes

--- src/components/Screens/user/user.js
+++ src/components/Screens/user/user.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const UserScreen = () => {
@@ -8,5 +8,11 @@ const UserScreen = () => {
     </View>
   )
 }
+
+const style = StyleSheet.create({
+  container: {
+    
+  }
+})
 
 export default UserScreen
